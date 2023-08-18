a3fbf9f48a57a2f0c96e975a949ab94dc704be53
youngkwonkim9164 <youngkwonkim9164@gmail.com>
2023-08-18 17:09:27 +0900

    변수 선언에 final을 붙여보자

--- main.dart
+++ main.dart
@@ -26,15 +26,25 @@ void main() {
  * Nullable
  */
 
-  bool isEmpty(String string) => string.length() == 0;
-  isEmpty(null);
+  // bool isEmpty(String string) => string.length() == 0;
+  // isEmpty(null);
 
   // 어떤 변수가 Null이 될수있음을 정확히 표시해줘야한다.
   // String jay = 'jay';
   // jay = null; // X
 
-  String? jay = 'jay'; // type 뒤에 ?를 붙이면 string또는 null이될수도 있다.
-  jay = null;
+  // String? jay = 'jay'; // type 뒤에 ?를 붙이면 string또는 null이될수도 있다.
+  // jay = null;
 
-  jay?.isNotEmpty; // jay가 null이 아닐때만 isNotEmpty를 준다.
+  // jay?.isNotEmpty; // jay가 null이 아닐때만 isNotEmpty를 준다.
+
+  /**
+   * Final Variables
+   */
+
+  String name = 'jay';
+  name = 'jay modif'; // String으로만 선언하면 수정이 가능하다.
+
+  final String final_name = 'jay'; // final 뒤에 타입선언해도 되지만 추론이 가능해서 보통 사용은 안함
+  final_name = 'jay modif'; // final로 선언하면 수정이 불가능하다.
 }
