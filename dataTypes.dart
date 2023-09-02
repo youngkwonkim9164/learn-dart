088e33c697f31c42d067cc2b26e65365ede8c301
youngkwonkim9164 <youngkwonkim9164@gmail.com>
2023-09-02 18:53:55 +0900

    datatypes of dart?

---
+++ dataTypes.dart
@@ -0,0 +1,12 @@
+void main() {
+  /**
+   * 대부분의 자료형들은 Object 이다.
+   * 심지어 function도 object이다.
+   */
+  String name = 'jay';
+  bool alive = true;
+  int age = 20;
+  double money = 10000000000;
+
+  num x = 123; // 이 Num은 double이 될수도있고 int가 될수도있다.
+}
