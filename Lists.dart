8e9339cda4f03be659a7926e798a9e730767fcfd
youngkwonkim9164 <youngkwonkim9164@gmail.com>
2023-09-04 17:50:45 +0900

    collection if in Lists

--- Lists.dart
+++ Lists.dart
@@ -3,16 +3,16 @@ void main() {
    * List
    */
 
-  List<int> numbers = [
+  var giveMeFive = true;
+  var numbers = [
     1,
     2,
     3,
     4,
-    5,
-    6, 
+    6,
     7, // 마지막원소에 , 를 붙이면 auto formatting 지원
+    if (giveMeFive) 5, // giveMeFive가 true라면 5를 추가한다. (collection if)
   ]; // integer 리스트
-  numbers.add('abc'); // string 추가 불가능
+  // numbers.add('abc'); // string 추가 불가능
   numbers.add(123); // integer 추가 가능
-
 }
