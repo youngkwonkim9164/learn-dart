999f9d4c966a1810e94966e0a0eeebde9caa9a25
youngkwonkim9164 <youngkwonkim9164@gmail.com>
2023-09-05 17:44:29 +0900

    collection for

---
+++ collection For.dart
@@ -0,0 +1,14 @@
+void main() {
+  /**
+   * collection for
+   */
+  var oldFriends = ['nico', 'lynn'];
+  var newFriends = [
+    'lewis',
+    'ralph',
+    'darren',
+    for (var friend in oldFriends) "awesom $friend"
+  ];
+
+  print(newFriends);
+}
