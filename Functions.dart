dce4a2244732f5ee0c28ead3718b9877bb05830e
youngkwonkim9164 <youngkwonkim9164@gmail.com>
2023-09-13 17:37:43 +0900

    add fat arrow function example

--- Functions.dart
+++ Functions.dart
@@ -3,14 +3,16 @@ void sayHello(String name) {
   print("Hello $name nice to meet you!");
 }
 
-
 // string을 return하고싶은 경우
 String sayHelloString(String name) {
   return ("Hello $name nice to meet you!");
 }
 
-// fat arrow function
+// fat arrow function (위 function과 같다.)
 String sayHelloTest(String name) => "Hello $name";
+
+// fat arrow function2 (a+b를 Return)
+num plus(num a, num b) => a + b;
 
 void main() {
   sayHello('Jay'); // Hello Jay nice to meet you!
