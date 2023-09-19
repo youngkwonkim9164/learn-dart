a13ee78f2cfb40a72a735318f9729e83ba8e5f7d
youngkwonkim9164 <youngkwonkim9164@gmail.com>
2023-09-19 16:05:28 +0900

    qq equal operator

--- !!!QQ operator.dart
+++ !!!QQ operator.dart
@@ -1,8 +1,3 @@
-void main() {
-  capitalizeName('jay');
-  capitalizeName(null); // null도 호출할 수 있도록 해주고싶을 때
-}
-
 // case 1: 일반적인 Null 체크 후 리턴 방법
 /**
 String capitalizeName(String? name) {
@@ -20,4 +15,22 @@ String capitalizeName(String? name) {
 */
 
 // case 3: qq 오퍼레이터를 이용한 방법
-String capitalizeName(String? name) => name.toUpperCase() ?? 'Anonymous';
+/**
+ * qq operator:  left ?? right 
+ * 왼쪽 항목이 Null이 아니면 왼쪽 항목을 Return
+ * 왼쪽 항목이 null이라면 오른쪽 항목을 return
+ */
+String capitalizeName(String? name) => name?.toUpperCase() ?? 'Anonymous';
+
+void main() {
+  capitalizeName('jay');
+  capitalizeName(null); // null도 호출할 수 있도록 해주고싶을 때
+
+/** qq equal opeator 
+ * 
+ * name ??= 'jay'; 
+ * name이 Null이라면 바로 'jay'를 대입한다.
+*/
+  String? name;
+  name ??= 'jay';
+}
