8299e27106dc230f6ca516902bae820f71fae1d2
youngkwonkim9164 <youngkwonkim9164@gmail.com>
2023-09-19 15:53:53 +0900

    설명 업데이트

--- OptionalPositionalParameters.dart
+++ OptionalPositionalParameters.dart
@@ -1,13 +1,12 @@
 String sayHello(
   String name,
   int age, [
-  String? country = "korea",
+  String? country = "korea", // optional하게 만들었다.
 ]) {
   return "Hello $name, you are $age, and you come from $country";
 }
 
 void main() {
   // sayHello('jay', 20, 'korea'); // default는 parameter가 require이지만
-  sayHello('jay',
-      20); // default는 parameter가 require이지만 [String? country='korea'] 로 optional하게 만들었다.
+  sayHello('jay', 20);
 }
