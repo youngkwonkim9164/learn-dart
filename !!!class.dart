43f7b4a88153adc313d4fc708348739547a5ea58
youngkwonkim9164 <youngkwonkim9164@gmail.com>
2023-09-27 21:26:10 +0900

    add explain

--- !!!class.dart
+++ !!!class.dart
@@ -20,9 +20,10 @@ class Player {
 
 void main() {
   var player = new Player("jay", 1000); //New는 필수가 아니다.
+  var player2 = new Player("jay2", 2000); // 생성자를 만들었기때문에 똑같은 Player 사용 가능
   print(player.name); // jay
   player.name = 'blabla';
-  print(player.name); // blabla
+  print(player2.name); // Jay2
 
   // player.country = 'abc'; // 수정 불가
   player.sayHello();
