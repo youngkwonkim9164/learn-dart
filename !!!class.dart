68af945f01554b65ba1a42c731b17bd7852eae72
youngkwonkim9164 <youngkwonkim9164@gmail.com>
2023-09-27 21:07:51 +0900

    생성자와 Late 명령어

--- !!!class.dart
+++ !!!class.dart
@@ -1,20 +1,25 @@
 /**
- * Class
+ * Class & Constructor
  * class에서 property를 선언할때는 타입을 사용해서 정의한다.
  */
 
 class Player {
-  String name = 'jay';
-  int xp = 1500;
-  final String country = 'seoul'; // 변경 불가하도록
+  late String name; // late 명령어: 변수들의 값을 나중에 받아올거다!
+  late int xp;
+
+  // 생성자
+  Player(String name, int xp) {
+    this.name = name;
+    this.xp = xp;
+  }
 
   void sayHello() {
-    print("Hi my name is $name, i came from $country");
+    print("Hi my name is $name");
   }
 }
 
 void main() {
-  var player = new Player(); //New는 필수가 아니다.
+  var player = new Player("jay", 1000); //New는 필수가 아니다.
   print(player.name); // jay
   player.name = 'blabla';
   print(player.name); // blabla
